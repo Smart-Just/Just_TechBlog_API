114dc119523629677e40da58d7afbf5653181cd1
novicePGT <dymnam@naver.com>
2022-11-09 14:04:41 +0900

    fix(geuntae): 패키지명, 프로젝트명 수정

---
+++ src/main/java/club/jjackr/project/TestClient.java
@@ -0,0 +1,11 @@
+package club.jjackr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClient {
+    public static void main(String[] args) {
+        SpringApplication.run(TestClient.class, args);
+    }
+}
